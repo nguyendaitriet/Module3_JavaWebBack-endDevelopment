07a9883a42c5bcb0e87aebcf7b0bb6a53f7e4bac
nguyendaitriet <triet9510@gmail.com>
2022-05-25 14:28:21 +0700

    Done

---
+++ Week3/New folder/ServerTime/src/main/java/com/triet/servertime/HelloServlet.java
@@ -0,0 +1,31 @@
+package com.triet.servertime;
+
+import java.io.*;
+import java.util.Date;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import javax.xml.crypto.Data;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+//    private String message;
+
+    public void init() {
+//        message = "Hello World!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+
+        Date today = new Date();
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + today + "</h1>");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+    }
+}
